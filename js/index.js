30b31516aeab6a9e70c9469a84fc4e6c9753c9fb
blackeagle94 <blackeagle4894@gmail.com>
2020-12-10 17:58:49 +0200

    added endpoint and api key

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@
 const input = document.querySelector('.search__field');
 const submitBtn = document.querySelector('.search__btn');
 
+const apiKey = '389c426d-8b3a-4701-946e-e2ce5a2d2060';
+const url = 'https://forkify-api.herokuapp.com/api/v2/recipes'
 
 // Add AJAX functions here:
 const getRecipe = async () => {
